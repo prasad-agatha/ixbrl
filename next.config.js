7caa8cf3bf1766743ad66b82230d84f7725eed5b
prasad-agatha <prasad@agathainnovation.com>
2025-06-01 12:04:32 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: false,
+  serverRuntimeConfig: {
+    PROJECT_ROOT: __dirname,
+  },
+  env: {
+    RECAPTCHA_SITE_KEY: process.env.RECAPTCHA_SITE_KEY,
+    AWS_S3_REGION: process.env.AWS_S3_REGION,
+    AWS_S3_ACCESS_KEY_ID: process.env.AWS_S3_ACCESS_KEY_ID,
+    AWS_S3_SECRET_ACCESS_KEY: process.env.AWS_S3_SECRET_ACCESS_KEY,
+    AWS_S3_BUCKET_NAME: process.env.AWS_S3_BUCKET_NAME,
+  },
+};
+
+module.exports = nextConfig;
